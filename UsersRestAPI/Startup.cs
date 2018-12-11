bd2917cc41266b9cfa0218365b0ef3047caacf47
Haline Ferreira <halineferreira@gmail.com>
2018-12-10 22:44:05 -0200

    Dependency injection of repositories

--- UsersRestAPI/Startup.cs
+++ UsersRestAPI/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.DependencyInjection;
 using UsersRestAPI.Model.Context;
 using UsersRestAPI.Business ;
 using UsersRestAPI.Business.Implementations;
+using UsersRestAPI.Repository;
+using UsersRestAPI.Repository.Implementations;
 
 namespace UsersRestAPI
 {
@@ -27,8 +29,11 @@ namespace UsersRestAPI
             services.AddMvc();
 
             services.AddScoped<IRoleBusiness, RoleBusinessImpl>();
+            services.AddScoped<IRoleRepository, RoleRepositoryImpl>();
             services.AddScoped<IPermissionBusiness, PermissionBusinessImpl>();
-            services.AddScoped<IUserBusiness, UserBusinessImpl>();        
+            services.AddScoped<IPermissionRepository, PermissionRepositoryImpl>();
+            services.AddScoped<IUserBusiness, UserBusinessImpl>();
+            services.AddScoped<IUserRepository, UserRepositoryImpl>();
         }
 
             // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
